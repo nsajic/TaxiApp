f860f4dd3eafdc537fef71e9f35fc1ede16a99b5
unknown <acasm94@gmail.com>
2018-06-12 21:59:35 +0200

    Added text for user guide

--- TaxiApp/app/src/main/java/com/example/nsajic/testapp/UserGuid.java
+++ TaxiApp/app/src/main/java/com/example/nsajic/testapp/UserGuid.java
@@ -9,7 +9,17 @@ import android.widget.TextView;
 
 public class UserGuid extends AppCompatActivity {
 
-    private final String upustvo =  "Izvadite batiriju kako biste ugasili aplikaciju. \nDa dozivotno zabranite aplikaciju na svom telefonu, bacite telefon u najblizu reku. \ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp\ntemp";
+    private final String upustvo =  "#Fragment GRADOVI: \n" +
+                                    "\t U ovom fragmentu prikazana je lista svih gradova u kojima se nalaze taxiji iz baze. \n" +
+                                    "\t * Odabirom jednog od grada izlistace se sve taxi sluzbe u okviru tog grada. \n" +
+                                    "\t * Taxi sluzbe moguce je dodati u omiljene klikom na zvezdicu ili selektovati za prikaz detalja \n" +
+                                    "\t * Ulaskom u detalje osim osnovnih informacija moguce je dati ocenu tom taxiju, dadati i pregledati recenzije i pozvati ga." +
+                                    "#Fragment OMILJENI TAXI: \n" +
+                                    "\t U ovom fragmentu prikazana je lista svih taxija koje ste dodali u omiljene. \n" +
+                                    "\t * Omiljene taxi sluzbe je moguce selektovati za prikaz detalja \n" +
+                                    "#Fragment STAJALISTA: \n" +
+                                    "\t U ovom fragmentu prikazana je mapa sa svim taxi stajalistima iz nase baze. Pomocu nje mozete videti gde se nalazi najblize taxi stajaliste. \n\n" +
+                                    "";
     private TextView upustvoTextView;
 
     @Override
