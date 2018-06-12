00c947a1eba74dd32e2d4684d6e8d16e0e3f46c0
OneZeroBeatz <onezerobeatz@gmail.com>
2018-06-12 21:39:24 +0200

    Specification added, small fix

--- TaxiApp/app/src/main/java/com/example/nsajic/testapp/MainActivity.java
+++ TaxiApp/app/src/main/java/com/example/nsajic/testapp/MainActivity.java
@@ -39,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         
         firebaseAuth = FirebaseAuth.getInstance();
