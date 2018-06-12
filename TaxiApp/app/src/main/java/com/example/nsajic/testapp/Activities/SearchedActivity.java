d1d5a44b5fdcb4b90075ccba50fc05cc661e52fa
unknown <acasm94@gmail.com>
2018-06-12 21:05:56 +0200

    Filtering is implemented

--- TaxiApp/app/src/main/java/com/example/nsajic/testapp/Activities/SearchedActivity.java
+++ TaxiApp/app/src/main/java/com/example/nsajic/testapp/Activities/SearchedActivity.java
@@ -9,8 +9,11 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.nsajic.testapp.Adapters.GradoviAdapter;
+import com.example.nsajic.testapp.Adapters.TaxiSluzbaAdapter;
 import com.example.nsajic.testapp.Models.Grad;
+import com.example.nsajic.testapp.Models.TaxiSluzba;
 import com.example.nsajic.testapp.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,8 +31,13 @@ public class SearchedActivity extends AppCompatActivity {
     private Double price;
 
     private DatabaseReference databaseReference;
+    private FirebaseAuth firebaseAuth;
 
     private ArrayList<Grad> cities;
+    private ArrayList<TaxiSluzba> taxiSluzbas;
+    ArrayList<String> omiljeneSluzbe = new ArrayList<String>();
+
+    private ListView lv;
 
     private Intent intent;
 
@@ -39,6 +47,8 @@ public class SearchedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_searched);
 
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+
         intent = getIntent();
 
         cityTaxi = intent.getStringExtra("cityTaxi");
@@ -62,7 +72,7 @@ public class SearchedActivity extends AppCompatActivity {
                         cities = cityByName(tempList, name);
                     }
 
-                    ListView lv = (ListView)findViewById(R.id.gradovi_listview);
+                    lv = (ListView)findViewById(R.id.finded_listview);
                     lv.setAdapter(new GradoviAdapter(cities, SearchedActivity.this));
 
                     lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -85,10 +95,60 @@ public class SearchedActivity extends AppCompatActivity {
 
 
         }else if(cityTaxi.equals((getResources().getStringArray(R.array.filter_spinner)[1]))){
-            databaseReference.child("gradovi").child(postanskiBroj).child("taxiSluzbe").addValueEventListener(new ValueEventListener() {
+            databaseReference.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
-                    List<Grad> cities = cityByName((ArrayList<Grad>)dataSnapshot.getValue(), name);
+                    //
+                    Iterable<DataSnapshot> taxiUids = dataSnapshot.child("gradovi").child(postanskiBroj).child("taxiSluzbe").getChildren();
+                    Iterable<DataSnapshot> omiljeneSluzbeDB = dataSnapshot.child("korisnici").getChildren();
+                    ArrayList<TaxiSluzba> tempList = new ArrayList<>();
+
+                    for (DataSnapshot taxiUidSnapsot : taxiUids) {
+                        tempList.add(taxiUidSnapsot.getValue(TaxiSluzba.class));
+                    }
+
+                    if(name.isEmpty() && price == 0){
+                        taxiSluzbas = tempList;
+                    }else{
+                        taxiSluzbas = taxiByNameAndPrice(tempList, name, price);
+                    }
+
+                    for(DataSnapshot coek: omiljeneSluzbeDB){
+                        if(coek.getKey().equals(firebaseAuth.getCurrentUser().getUid())) {
+                            for (DataSnapshot sluzba : coek.child("omiljeneSluzbe").getChildren()) {
+                                omiljeneSluzbe.add(sluzba.getValue(String.class));
+                            }
+                        }
+                    }
+
+                    for(TaxiSluzba ts : taxiSluzbas){
+                        if (omiljeneSluzbe.contains(ts.getIme())) {
+                            ts.setFavouriteChecked(true);
+                        } else {
+                            ts.setFavouriteChecked(false);
+                        }
+                    }
+
+                    lv = (ListView)findViewById(R.id.finded_listview);
+                    lv.setAdapter(new TaxiSluzbaAdapter(taxiSluzbas, SearchedActivity.this));
+
+                    lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            //CheckBox cb = (CheckBox) view.findViewById((R.id.favouriteChecked));
+                            Intent intent = new Intent(view.getContext(), TaxiInfoActivity.class);
+
+                            intent.putExtra("imeSluzbe", taxiSluzbas.get(i).getIme());
+                            intent.putExtra("brojTelefona", taxiSluzbas.get(i).getBrojTelefona());
+                            intent.putExtra("ocenaSluzbe", taxiSluzbas.get(i).getOcena().toString());
+                            intent.putExtra("brojAutomobilaSluzbe", taxiSluzbas.get(i).getBrojAutomobila().toString());
+                            intent.putExtra("cenaPoKilometru", taxiSluzbas.get(i).getCenaPoKilometru().toString());
+
+                            startActivity(intent);
+                        }
+                    });
+
                 }
 
                 @Override
@@ -102,10 +162,25 @@ public class SearchedActivity extends AppCompatActivity {
     private ArrayList<Grad> cityByName(List<Grad> cities, String name){
 
         ArrayList<Grad> retVal = new ArrayList<>();
-        for (Grad c :
-                cities) {
-            if(c.getIme().toLowerCase().contains(name.toLowerCase())){
+        name = name.toLowerCase();
+
+        for (Grad c : cities) {
+            if(c.getIme().toLowerCase().contains(name)){
                 retVal.add(c);
+            }
+        }
+
+        return retVal;
+    }
+
+    private ArrayList<TaxiSluzba> taxiByNameAndPrice(List<TaxiSluzba> taxiSluzbe, String name, Double price){
+
+        ArrayList<TaxiSluzba> retVal = new ArrayList<>();
+        name = name.toLowerCase();
+
+        for (TaxiSluzba ts : taxiSluzbe) {
+            if(ts.getIme().toLowerCase().contains(name) && ts.getCenaPoKilometru() <= price){
+                retVal.add(ts);
             }
         }
 
